5b4a3e94acf9c7d032c704ff53281d9b7d7249e0
rlaqjatjr8922 <gimbeomseognam5@gmail.com>
2026-03-31 16:44:55 +0900

    자동 저장

--- run.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-print("RUN FILE =", Path(__file__).resolve())
-
-from src.pipeline.pregame_pipeline import run_pregame_pipeline
-
-if __name__ == "__main__":
-    run_pregame_pipeline()
